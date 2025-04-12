d84544dafdad3e332eabf507ca635aadf80811c1
梦泽 <administer@mengze.vip>
2025-04-12 14:22:10 +0800

    更新 Cta.vue

--- components/home/Cta.vue
+++ components/home/Cta.vue
@@ -3,10 +3,10 @@
     class="flex flex-col items-center max-w-4xl p-8 mx-auto my-12 text-center bg-black rounded-lg md:px-20 md:py-20"
   >
     <h2 class="text-4xl tracking-tight text-white md:text-6xl">
-      {{ $t('home.cta.title') }}
+      加入本站QQ群
     </h2>
     <p class="mt-4 text-lg text-slate-400 md:text-xl">
-      {{ $t('home.cta.description') }}
+      加入梦泽的QQ群，群里啥都聊，还有各路大佬祝你开发之路一路畅通
     </p>
     <div class="flex mt-5">
       <HomeLink
@@ -14,7 +14,7 @@
         type="inverted"
         :title="$t('home.cta.button')"
       >
-        {{ $t('home.cta.button') }}
+        立即加入
       </HomeLink>
     </div>
   </div>
