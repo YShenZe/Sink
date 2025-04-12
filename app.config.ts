8c4adc5aeadfc6d8eca93b38fb232cd58449f3bb
梦泽 <administer@mengze.vip>
2025-04-12 14:08:09 +0800

    更新 app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -7,7 +7,7 @@ export default defineAppConfig({
   mastodon: 'https://mzurl.xyz/',
   blog: 'https://mzurl.xyz/kf5gq9',
   description: '简单/快速/安全的链接缩短器，可进行分析，100% 在 Cloudflare 上运行。',
-  image: 'https://sink.cool/banner.png',
+  image: 'https://cdn.mengze.vip/gh/YShenZe/Blog-Static-Resource@main/images/1f94e9c693374150b1f8dfd8de0fcce1.jpeg',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
